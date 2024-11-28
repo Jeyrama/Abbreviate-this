5365521a52396c3759b7cdcba8aa62de0e3ef225
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-11-27 22:36:02 -0800

    Update Abbreviate-this.js

--- Abbreviate-this.js
+++ Abbreviate-this.js
@@ -57,3 +57,10 @@ function i18n(word) {
 }
 
 // or
+
+let find = /[a-z]{4,}/gi;
+function replace(match) { return match[0] + (match.length - 2) + match[match.length - 1]; }
+
+function abbreviate(string) {
+  return string.replace(find, replace);
+}
